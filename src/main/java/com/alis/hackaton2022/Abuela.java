8f9407be9238b8a002e5a43e6bdfc6d54c9aad5a
Allecan <allecan52@gmail.com>
2022-11-05 16:19:55 -0600

    abuela terminado

--- src/main/java/com/alis/hackaton2022/Abuela.java
+++ src/main/java/com/alis/hackaton2022/Abuela.java
@@ -12,15 +12,22 @@ public class Abuela {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
         System.out.print("Ingresa el total: ");
         Integer N = in.nextInt();
         System.out.print("Ingresa el primer numero: ");
         Integer num1 = in.nextInt();
         System.out.print("Ingresa el segundo numero: ");
         Integer num2 = in.nextInt();
-        System.out.println(num1.byteValue());
-        System.out.println(num2.byteValue());
+        String strBytes = Integer.toBinaryString(num1) + Integer.toBinaryString(num2);
+        char[] arrBytes = strBytes.toCharArray();
+        int cont = 0;
+        for (int i = 0; i < arrBytes.length; i++) {
+            if (arrBytes[i] == '1') {
+                cont += 1;
+            }
+        }
+        System.out.println("La abuela le dara a su nieta: " + cont + " avellanas");
     }
-    
+
 }
